a849cd975f6332803f224e0770659619609176b9
Pranav Singh <developer.pranav@gmail.com>
2015-01-07 01:56:03 +0530

    Sets git is source control in capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, 'git@bitbucket.org:pranav7/smartpaddle-ruby.git'
 set :deploy_to, '/home/ubuntu/smartpaddle_app'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty

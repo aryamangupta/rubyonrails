6f6db22d08636931626f5a17c57149a2c4ce485f
Pranav Singh <developer.pranav@gmail.com>
2015-01-10 23:07:39 +0530

    Removes create database task from executing

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,7 +88,6 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
-  before :finishing,    :create_database
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart

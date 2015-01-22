56b2a6c83d2c622534f4482ed7f8034c5426d7a2
root <ankittomar@gmail.com>
2015-01-23 01:50:07 +0530

    Show Product Page done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,2 @@
 class PicturesController < ApplicationController
-  def create
-    @product = Product.find(params[:product_id])
-    @picture = @product.pictures.create(:caption => "",
-      									:image => params[:product][:pictures][:image])
-    redirect_to article_path(@product)
-  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,10 @@ class ProductsController < ApplicationController
 	def new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def create
 
   	$moredetails = "de"
